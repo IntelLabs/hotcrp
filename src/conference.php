a60ccab06397f935d07a3d110f8dad6786d58e16
Michael Beale <michael.beale@intel.com>
2021-10-20 09:04:41 -0700

    fixed syntax error

--- src/conference.php
+++ src/conference.php
@@ -4059,7 +4059,7 @@ class Conf {
         <!--[if lte IE 9]>
             <meta http-equiv=\"X-UA-Compatible\" content=\"IE=9\"/>
         <![endif]-->
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">;
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=yes\">";
         
         if (($font_script = $this->opt("fontScript"))) {
             if (!str_starts_with($font_script, "<script")) {
